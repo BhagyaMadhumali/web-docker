461c3bafeec48e62922ffda6a2bf5f57940c2f6a
BhagyaMadhumali <madhumalibhagya122@gmail.com>
2026-01-30 18:38:48 +0530

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,6 +59,6 @@ resource "aws_instance" "web_server" {
 
 # Outputs
 output "instance_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.web_server.public_ip
+  value = aws_instance.my_instance.public_ip
 }
+
