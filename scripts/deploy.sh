7b3f5039c842ccf5aaaedc31762ff0481bd43480
BhagyaMadhumali <madhumalibhagya122@gmail.com>
2026-01-30 21:22:14 +0530

    updated for final deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,9 @@ echo "🚀 Deploying containers using Docker Compose..."
 # Go to project root
 cd "$(dirname "$0")/.."
 
+# Log in to Docker Hub
+docker login -u bhagya122 -p $DOCKERHUB_PASS
+
 # Stop old containers
 docker compose down
 
