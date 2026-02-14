7d55f91492b4c652d56e4259abee1f76d73b39eb
BhagyaMadhumali <madhumalibhagya122@gmail.com>
2026-02-14 12:54:21 +0530

    change job seeker

--- frontend/src/jobseekerheader/jobseekerheader.jsx
+++ frontend/src/jobseekerheader/jobseekerheader.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./jobseekerheader.css";
+import '../jobseekerheader/jobseekerheader.css';
+
 
 const Jobseekerheader = () => {
   const navigate = useNavigate();
